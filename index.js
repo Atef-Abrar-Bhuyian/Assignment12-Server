966181e3f204edf713b032b3f79cb42324b7af54
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-12-31 04:53:05 +0600

    need volunteer collection get from mongodb

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-
-
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.68dnu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -15,23 +17,32 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const needVolunteer = client.db("volunteerDB").collection("needVolunteer");
+
+    app.get("/needVolunteer", async (req, res) => {
+      const cursor = needVolunteer.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("VolunVibe Server Is Running!");
