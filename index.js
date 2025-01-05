de5b563de684d027e78cd9fe38e4ef97ee5a3e9f
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-06 00:59:56 +0600

    client side deployed to production and deploy link added to corsOptions's origin

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const cookieParser = require("cookie-parser");
 
 // whitelisted URL's
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://volunvibe.web.app",
+    "https://volunvibe.firebaseapp.com",
+  ],
   credentials: true,
 };
 
@@ -217,12 +221,12 @@ async function run() {
     });
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
