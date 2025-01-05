28a6d8152b28622ab8727d496fe781f914e93742
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-05 18:21:37 +0600

    jwt token generate done and clear cookie functionalities done after logout

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.68dnu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,6 +32,33 @@ async function run() {
     const volunteerRequest = client
       .db("volunteerDB")
       .collection("volunteerRequest");
+
+    // generate JWT
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      // create token
+      const token = jwt.sign(email, process.env.SECRET_TOKEN_KEY, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    // logout and clear cookie from browser
+    app.get("/jwtLogout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // get all volunteer post
     app.get("/needVolunteer", async (req, res) => {
